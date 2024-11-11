6291757ffb311d55f0d881afd057be63c44c1def
Zergulina <antonchernyaevt@gmail.com>
2024-11-11 21:33:37 +0300

    Calculations added

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+while (true)
+{
+    string a = Console.ReadLine();
+    Console.WriteLine(MathOperationsService.Calculate(a.ToRPN()));
+}
